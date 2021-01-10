bbcaad0ba3205438f18c5641c2a9c30dc74f014f
Akram Sabbah <akramsabbah9@gmail.com>
2021-01-10 02:11:08 -0800

    display current date and color-code time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,58 @@
 /* GLOBALS */
 // array to hold schedule of events.
-// each event looks like: {timeblock: hour, event: "string" }
-var schedule = [];
-
-// vars to hold set intervals
-var now, daily;
+// each event looks like: {time: hour, event: "string" }
+var schedule = [{time:  9, event: ""}, {time: 10, event: ""}, {time: 11, event: ""}, 
+                {time: 12, event: ""}, {time: 13, event: ""}, {time: 14, event: ""}, 
+                {time: 15, event: ""}, {time: 16, event: ""}, {time: 17, event: ""}];
 
 
 /* FUNCTIONS */
-// displays 
+// Update the page based on what time it is.
+var updatePage = function () {
+    var now = moment();
+    
+    // 1) change the date display if it needs changing
+    $("#currentDay").text(now.format("dddd, MMMM Do"));
 
-// function to get schedule from localStorage
+    // 2) highlight present/past/future hour slots
+    var currentHour = parseInt( now.format("H") );
+    for(var i = 9; i <= 17; i++) {
+        var hourID = "#time-" + i;
+        
+        // remove the highlight classes from this time block
+        $(hourID).removeClass("past present future");
+
+        // then, add the appropriate highlight class
+        if (i < currentHour)        $(hourID).addClass("past");
+        else if (i === currentHour) $(hourID).addClass("present");
+        else                        $(hourID).addClass("future");
+    }
+};
+
+// TODO: function to update schedule and localStorage
+
+// Get schedule from localStorage.
+var loadSchedule = function () {
+    ;
+};
+
+
 
 /* EVENT LISTENERS */
-// event listener for 
-// event listener for clicking on event textboxes
+// TODO: event listener for clicking on event textboxes
 
-// event listener for clicking off event textboxes (save to localstorage)
+// TODO: event listener for clicking save buttons
 
-// event listener for all save buttons (and/or form submissions)
+
 
 /* MAIN */
 // load events into schedule from localStorage
 
-// check and display date
+// perform initial update on the page
+updatePage();
 
-// every day, change displayed date and reset schedule
-/*
-1: calc how much time until the next day
-2: make a setInterval function that updates the 
+// every five minutes, update the page
+setInterval(updatePage, 1000 * 60 * 5);
 
 
 /* Akram Sabbah */
