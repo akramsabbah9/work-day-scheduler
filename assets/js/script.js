0ddb9677937d6df8f39000862c38a3bfeda044ee
Akram Sabbah <akramsabbah9@gmail.com>
2021-01-09 20:40:01 -0800

    more initial stuff

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+/* GLOBALS */
+// array to hold schedule of events.
+// each event looks like: {timeblock: hour, event: "string" }
+var schedule = [];
+
+
+/* FUNCTIONS */
+// function to get schedule from localStorage
+
+// function to display current date
+
+/* EVENT LISTENERS */
+// event listener for clicking on event textboxes
+
+// event listener for clicking off event textboxes (save to localstorage)
+
+// event listener for all save buttons (and/or form submissions)
+
+/* MAIN */
+// load events into schedule from localStorage
+
+// 
+
+// need something to reset the schedule each day to blank form, 
+
+
+/* Akram Sabbah */
